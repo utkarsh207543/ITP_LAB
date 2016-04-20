a0037b5fa04afafc65aa58561112454dcd20e606
Utkarsh Kumar Singh <utkarsh207543@gmail.com>
2016-04-20 11:34:23 +0530

    Create program 7.C

---
+++ program 7.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+ int main()
+ {
+  printf("%d \n",sizeof(int));
+  printf("%d \n",sizeof(char));
+  printf("%d \n",sizeof(float));
+  printf("%d \n",sizeof(double));
+ 
+  system("pause");
+  return 0;
+ }
